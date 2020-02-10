ab6d2ecf21e29339a12a4abc052e5fd9a6d388c9
SunXiaojie <505932690@qq.com>
2020-02-10 20:21:05 +0800

    student

--- DormBackend/models.py
+++ DormBackend/models.py
@@ -10,8 +10,10 @@ class Student(models.Model):
     tel = models.CharField(max_length=16, null=True, default="")
     tutor = models.ForeignKey("Teacher", on_delete=models.SET_NULL, null=True)
     college = models.CharField(max_length=32, null=True, default="")  # 学院
-    major = models.CharField(max_length=32, null=True, default="")  # 专业
-    room = models
+    detail = models.CharField(max_length=32, null=True, default="")  # 专业
+    room = models.ForeignKey("Room", on_delete=models.SET_NULL, null=True)
+    home_address = models.TextField(null=True, default="")
+    name = models.CharField(max_length=32, null=True, default="")
 
 
 class Teacher(models.Model):
@@ -22,12 +24,26 @@ class Teacher(models.Model):
 
 
 class Room(models.Model):
-    room_id = models.CharField(max_length=32, null=True, default="")
-    build = models.CharField(max_length=32, null=True, default="")
-    result = models.CharField(max_length=32, null=True, default="")  # 检查结果
-    comment = models.CharField(max_length=32, null=True, default="")  # 备注
+    room_id = models.CharField(max_length=32, null=True, default="")  # 宿舍号
+    build = models.CharField(max_length=32, null=True, default="")  # 楼号
+    capacity = models.IntegerField(default=0)  # 实际已住人员数量
 
-class Account(models.Model):
+
+class ManagerAccount(models.Model):
     account_name = models.ForeignKey("Teacher", on_delete=models.CASCADE)
     pwd = models.CharField(max_length=32, default="123456")
-    level = models.IntegerField( default="1")
+    level = models.IntegerField(default="1")
+
+
+class StuAccount(models.Model):
+    account_name = models.ForeignKey("Student", on_delete=models.CASCADE)
+    pwd = models.CharField(max_length=32, default="123456")
+
+
+class InspectionHistory(models.Model):
+    year = models.IntegerField(default=2020)
+    month = models.IntegerField(default=1)
+    day = models.IntegerField(default=1)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    result = models.CharField(max_length=32, null=True, default="")  # 检查结果
+    comment = models.CharField(max_length=32, null=True, default="")  # 备注
--- DormBackend/urls.py
+++ DormBackend/urls.py
@@ -2,9 +2,14 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from DormBackend.views import *
+from student import views as sv
 
 urlpatterns = [
     url(r'^index$', index),
     url(r'^db_config$', db_config),
-    url(r'^config$', config)
+    url(r'^config$', config),
+    url(r'^student_info$', sv.student_info),
+    url(r'^change_stu_info$', sv.change_stu_info),
+    url(r'^mate_info$', sv.mate_info),
+    url(r'^clean_info$', sv.clean_info),
 ]
