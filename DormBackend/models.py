04a68f8c4bb797ccfc68fab72ec2a4549d286e5e
SunXiaojie <505932690@qq.com>
2020-02-11 12:11:10 +0800

    student

---
+++ DormBackend/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+# Create your models here.
+from django.db.models import Model
+
+
+class Student(models.Model):
+    stu_id = models.CharField(max_length=32, null=True, default="")
+    emergence_tel = models.CharField(max_length=16, null=True, default="")
+    tel = models.CharField(max_length=16, null=True, default="")
+    tutor = models.ForeignKey("Teacher", on_delete=models.SET_NULL, null=True)
+    college = models.CharField(max_length=32, null=True, default="")  # 学院
+    detail = models.CharField(max_length=32, null=True, default="")  # 具体信息
+    room = models.ForeignKey("Room", on_delete=models.SET_NULL, null=True)
+    home_address = models.TextField(null=True, default="")
+    name = models.CharField(max_length=32, null=True, default="")
+
+
+class Teacher(models.Model):
+    tea_id = models.CharField(max_length=32, null=True, default="")
+    name = models.CharField(max_length=16, null=True, default="")
+    college = models.CharField(max_length=32, null=True, default="")
+    tel = models.CharField(max_length=16, null=True, default="")
+
+
+class Room(models.Model):
+    room_id = models.CharField(max_length=32, null=True, default="")
+    build = models.CharField(max_length=32, null=True, default="")
+    capacity = models.IntegerField(default=0)   # 实际已住
+
+
+class ManagerAccount(models.Model):
+    account_name = models.ForeignKey("Teacher", on_delete=models.CASCADE)
+    pwd = models.CharField(max_length=32, default="123456")
+    level = models.IntegerField( default="1")
+
+
+class StuAccount(models.Model):
+    account_name = models.ForeignKey("Student", on_delete=models.CASCADE)
+    pwd = models.CharField(max_length=32, default="123456")
+
+
+class InspectionHistory(models.Model):
+    year = models.IntegerField(default=2020)
+    month = models.IntegerField(default=1)
+    day = models.IntegerField(default=1)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    result = models.CharField(max_length=32, null=True, default="")  # 检查结果
+    comment = models.CharField(max_length=32, null=True, default="")  # 备注
+
+
+class Warning(models.Model):
+    sponsor = models.ForeignKey("Teacher", on_delete=models.SET_NULL, null=True)
+    room = models.ForeignKey("Room", on_delete=models.CASCADE, null=True)
+    level = models.CharField(max_length=32, null=True, default="")
+    comment = models.CharField(max_length=512, null=True, default="")
---
+++ main.py
@@ -0,0 +1,57 @@
+import json
+import os
+import sys
+import threading
+from time import sleep
+from traceback import print_exc
+from logs.log import Log
+import eel
+import pymysql
+
+
+def start_django():
+    # 判断操作系统类型
+    sys_name: str = os.name.upper()
+    if sys_name.startswith("NT"):  # windows系列
+        os.system("python manage.py makemigrations")
+        os.system("python manage.py migrate")
+        os.system("python manage.py runserver 8000")
+    elif sys_name.startswith("POSIX"):  # unix linux 系列
+        os.system("python3 manage.py makemigrations")
+        os.system("python3 manage.py migrate")
+        os.system("python3 manage.py runserver 8000")
+
+
+def start_eel():
+    # 启动的函数调用放在最后,port=0表示使用随机端口,size=(宽,高)
+    eel.start('db.html', port=8001, size=(1024, 796))
+
+
+if __name__ == "__main__":
+    # html文件所在文件夹
+    eel.init('DormFrontend')
+    # 检测数据库配置
+    Log.i(__name__, "开始检查数据库配置")
+
+
+    # 调用线程
+    try:
+        Log.i(__name__, "开始启动django")
+        django = threading.Thread(target=start_django)
+        django.setDaemon(True)
+        django.start()
+    except pymysql.err.InternalError:
+        print_exc()
+        Log.e(__name__, "数据库配置错误, django启动失败")
+        sys.exit(0)
+    
+    sleep(0.5)
+    Log.i(__name__, "开始尝试启动EEL")
+    eel_t = threading.Thread(target=start_eel)
+    eel_t.start()
+    eel_t.setDaemon(False)
+    Log.i(__name__, "EEL启动成功")
+    eel_t.join()
+
+
+
